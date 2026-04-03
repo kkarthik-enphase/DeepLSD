5f9ed323fe1b9caecde9348e2d963b410d289a83
kkarthik319 <133770332+kkarthik319@users.noreply.github.com>
2026-04-03 14:50:19 +0530

    update DATA_PATH and EXPER_PATH to harddrive location

--- deeplsd/settings.py
+++ deeplsd/settings.py
@@ -2,5 +2,5 @@
     Modify `EXPER_PATH` and `DATA_PATH` to point to any directories,
     with absolute or relative paths.
 """
-EXPER_PATH = '/home/kkarthik/ARD/DeepLSD/experiments/'
-DATA_PATH = '/home/kkarthik/ARD/DeepLSD/data/'
+EXPER_PATH = '/home/amkumar/Desktop/harddrive/karthik/data/ARD/experiments/'
+DATA_PATH = '/home/amkumar/Desktop/harddrive/karthik/data/ARD/'
